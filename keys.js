0d4af1271e3201de555f93476ed6dea68c6c53f3
Leigh Fisher <leighfisher@Jamess-MacBook-Air-2.local>
2017-03-29 14:06:20 -0500

    initial site files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'ITqr7YIarpazkzW1CF9FBkOW6',
+  consumer_secret: 'ETscww8CbDiY4O4RxuCAlo7cXZIDuoFi7HceUEZ1nXaUURodwH',
+  access_token_key: '1219792934-yLe33VfunpLJY4GlMoHrv2l5PP9gQxWP9YZCFuD',
+  access_token_secret: 'zMsDtU0L71XhDVvY6sEYJoFJAKY5L6M6WaxNAupfbPBF8',
+}
